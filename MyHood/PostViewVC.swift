ed135c95d30e3769348a12bde7a0aba967cd5a94
Julian Mino <julianmino@Julians-Mac.local>
2019-03-21 17:44:24 -0300

    started adding new own things to the app: showing one of the posts in another window.

---
+++ MyHood/PostViewVC.swift
@@ -0,0 +1,34 @@
+//
+//  PostViewVC.swift
+//  MyHood
+//
+//  Created by Julian Mino on 3/21/19.
+//  Copyright © 2019 Julian Mino. All rights reserved.
+//
+
+import UIKit
+
+class PostViewVC: UIViewController {
+    
+    @IBOutlet weak var finalPost: UIImageView!
+
+    @IBOutlet weak var finalTitleLbl: UILabel!
+    
+    @IBOutlet weak var finalDescLbl: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    }
+
+}
